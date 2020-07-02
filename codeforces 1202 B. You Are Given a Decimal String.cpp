42258af4cd63d4bab0cbd80aa76567dc841ed654
Soumik Roy <39247393+soumik9876@users.noreply.github.com>
2020-07-02 17:19:50 +0600

    Add files via upload

---
+++ codeforces 1202 B. You Are Given a Decimal String.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#define ll                    long long int
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define MOD                   1000000007
+#define vi                    vector<int>
+#define vll                   vector<ll>
+#define pll                   pair<ll,ll>
+#define pii                   pair<int,int>
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define tcase()               ll t,n; cin>>t;n=t; while(t--)
+#define iscn(num)             scanf("%d",&num);
+#define eb                    emplace_back
+#define ull                   unsigned long long
+#define KAMEHAMEHA            ios_base::sync_with_stdio(0);
+#define RASENGAN              ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    KAMEHAMEHA
+//    #ifdef _soumik
+//        freopen("input.txt", "r", stdin);
+//    #endif
+    string s;
+    cin>>s;
+    int matrix[11][11];
+    for(int i=0;i<10;i++)
+    {
+        for(int j=0;j<10;j++)
+        {
+            int total=0,current=0;
+            for(auto i:s)
+            {
+                int diff=(i-'0')-current;
+                if(diff<0)
+                    diff+=10;
+
+            }
+        }
+    }
+    return 0;
+}
+
+
