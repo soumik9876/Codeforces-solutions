ec439780a7a9e6fb235ae9ca2e225210cd5c77ff
Soumik Roy <39247393+soumik9876@users.noreply.github.com>
2020-09-05 19:57:12 +0600

    Create codeforces 1401 A. Distance and Axis.cpp

---
+++ codeforces 1401 A. Distance and Axis.cpp
@@ -0,0 +1,54 @@
+//Author - Soumik Roy
+//Date & Time - September 05, 2020 12:21 PM
+//Problem name - A. Distance and Axis
+//Problem url - https://codeforces.com/contest/1401/problem/A
+//Time limit - 1000 ms
+//Memory Limit - 256 MB    
+
+#include<bits/stdc++.h>
+#define ll                    long long int
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define MOD                   1000000007
+#define vi                    vector<int>
+#define vll                   vector<ll>
+#define pll                   pair<ll,ll>
+#define pii                   pair<int,int>
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define eb                    emplace_back
+#define ull                   unsigned long long
+#define bug(x)				  cout<<"  [ "#x<<" = "<<x<<" ]"<<endl;
+#define KAMEHAMEHA            ios_base::sync_with_stdio(0);
+#define RASENGAN              ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int dir[]={0,1,0,-1,1,0,-1,0};
+void solve()
+{
+	int n,k;
+	cin>>n>>k;
+	if(n<=k)
+	{
+		cout<<k-n<<endl;
+		return;
+	}
+	else
+	{
+		cout<<((n%2)^(k%2))<<endl;
+	}
+	
+}
+int main()
+{
+    KAMEHAMEHA
+	int t=1;
+	cin>>t;
+	for(int cn=1;cn<=t;cn++)
+	{
+		solve();
+	}
+    return 0;
+}
+
