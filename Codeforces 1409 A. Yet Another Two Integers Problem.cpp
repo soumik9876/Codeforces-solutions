b60705f7c9046a768c019062db7509e507c50149
Soumik Roy <39247393+soumik9876@users.noreply.github.com>
2020-09-05 12:08:35 +0600

    Create Codeforces 1409 A. Yet Another Two Integers Problem.cpp

---
+++ Codeforces 1409 A. Yet Another Two Integers Problem.cpp
@@ -0,0 +1,47 @@
+//Author - Soumik Roy
+//Date & Time - September 04, 2020 8:35 PM
+//Problem name - A. Yet Another Two Integers Problem
+//Problem url - https://codeforces.com/contest/1409/problem/0
+//Time limit - 1000 ms
+//Memory Limit - 256 MB    
+
+#include<bits/stdc++.h>
+#define ll                    long long int
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define MOD                   1000000007
+#define vi                    vector<int>
+#define vll                   vector<ll>
+#define pll                   pair<ll,ll>
+#define pii                   pair<int,int>
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define eb                    emplace_back
+#define ull                   unsigned long long
+#define bug(x)				  cout<<"  [ "#x<<" = "<<x<<" ]"<<endl;
+#define KAMEHAMEHA            ios_base::sync_with_stdio(0);
+#define RASENGAN              ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int dir[]={0,1,0,-1,1,0,-1,0};
+void solve()
+{
+	ll a,b;
+	cin>>a>>b;
+	ll ans=abs(a-b);
+	ans=ceil((double)ans/10);
+	cout<<ans<<endl;
+}
+int main()
+{
+    KAMEHAMEHA
+	int t=1;
+	cin>>t;
+	for(int cn=1;cn<=t;cn++)
+	{
+		solve();
+	}
+    return 0;
+}
+
