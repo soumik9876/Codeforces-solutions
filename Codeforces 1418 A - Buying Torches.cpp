96f1ea98a49f035d0d02a9161fb64aeb9dcdde71
Soumik Roy <39247393+soumik9876@users.noreply.github.com>
2020-09-27 21:44:45 +0600

    Create Codeforces 1418 A - Buying Torches.cpp

---
+++ Codeforces 1418 A - Buying Torches.cpp
@@ -0,0 +1,49 @@
+//Author - Soumik Roy
+//Date & Time - September 27, 2020 9:10 AM
+//Problem name - A - Buying Torches
+//Time limit - 1000 ms
+//Memory Limit - 262 MB    
+//Parsed with ai-virtual-assistant https://github.com/Saurav-Paul/AI-virtual-assistant-python
+
+#include<bits/stdc++.h>
+#define ll                    long long int
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define MOD                   1000000007
+#define vi                    vector<int>
+#define vll                   vector<ll>
+#define pll                   pair<ll,ll>
+#define pii                   pair<int,int>
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define eb                    emplace_back
+#define ull                   unsigned long long
+#define bug(x)				  cout<<"  [ "#x<<" = "<<x<<" ]"<<endl;
+#define KAMEHAMEHA            ios_base::sync_with_stdio(0);
+#define RASENGAN              ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int dir[]={0,1,0,-1,1,0,-1,0};
+void solve()
+{
+	ll x,y,k;
+	cin>>x>>y>>k;
+	ll temp = x-1;
+	ll total = (y*k)+k-1;
+	ll trade = (total+temp-1)/(temp);
+	trade += k;
+	cout<<trade<<endl;
+}
+int main()
+{
+    KAMEHAMEHA
+	int t=1;
+	cin>>t;
+	for(int cn=1;cn<=t;cn++)
+	{
+		solve();
+	}
+    return 0;
+}
+
