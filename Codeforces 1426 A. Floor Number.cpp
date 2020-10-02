7a7895e8dc0bce366eb970f917e7e67086fd9f30
Soumik Roy <39247393+soumik9876@users.noreply.github.com>
2020-10-02 16:19:57 +0600

    Create Codeforces 1426 A. Floor Number.cpp

---
+++ Codeforces 1426 A. Floor Number.cpp
@@ -0,0 +1,52 @@
+//Author - Soumik Roy
+//Date & Time - October 02, 2020 11:55 AM
+//Problem name - A. Floor Number
+//Problem url - https://codeforces.com/contest/1426/problem/A
+//Time limit - 1000 ms
+//Memory Limit - 256 MB    
+//Parsed with ai-virtual-assistant https://github.com/Saurav-Paul/AI-virtual-assistant-python
+
+#include<bits/stdc++.h>
+#define ll                    long long int
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define MOD                   1000000007
+#define vi                    vector<int>
+#define vll                   vector<ll>
+#define pll                   pair<ll,ll>
+#define pii                   pair<int,int>
+#define all(p)                p.begin(),p.end()
+#define mid(s,e)              (s+(e-s)/2)
+#define eb                    emplace_back
+#define ull                   unsigned long long
+#define bug(x)				  cout<<"  [ "#x<<" = "<<x<<" ]"<<endl;
+#define KAMEHAMEHA            ios_base::sync_with_stdio(0);
+#define RASENGAN              ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int dir[]={0,1,0,-1,1,0,-1,0};
+void solve()
+{
+	int n,x;
+	cin>>n>>x;
+	if(n<=2)
+	{
+		cout<<1<<endl;
+		return;
+	}
+	int ans = ceil((double)(n-2)/x)+1;
+	cout<<ans<<endl;
+}
+int main()
+{
+    KAMEHAMEHA
+	int t=1;
+	cin>>t;
+	for(int cn=1;cn<=t;cn++)
+	{
+		solve();
+	}
+    return 0;
+}
+
